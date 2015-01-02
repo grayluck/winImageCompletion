01fb08da6ae26c81bcaba09add8b36154e902306
Gerald Zhang <monocofe@gmail.com>
2015-01-02 11:43:35 +0800

    copy to destination is finished. now work on Poisson interpolation

--- imgCompletion/imageEditing.cpp
+++ imgCompletion/imageEditing.cpp
@@ -11,24 +11,39 @@ namespace poi
 	Mat cov;	// a cov layer for img Src
 	Mat imgDispSrc;	// the image actually displayed as source img
 	Mat imgDispDest;
+	Mat res;	// save the process result
 	
 	int srcw, srch;
 	int destw, desth;
 
-	int mode;
+	int mode = 0;	// 0: copy; 1: poisson(no holes); 2: poisson(holes)
 	
 	int moused = 0;
 	int mousex, mousey;
 	
 	int brushr = 20;
+	
+	int destOffsetX, destOffsetY;
 
 	void redraw()
 	{
 		imshow(titleSrc, imgDispSrc);
+		imgDispDest = imgDest.clone();
+		for(int i = 0; i < destw; ++i)
+			for(int j = 0; j < desth; ++j)
+			{
+				int x = i - destOffsetX;
+				int y = j - destOffsetY;
+				if( x < 0 || y < 0 || x >= srcw || y >= srch)
+					continue;
+				Vec3b& vec = cov.at<Vec3b>(Point(x, y));
+				if(!vec[0])
+					imgDispDest.at<Vec3b>(Point(i, j)) = imgSrc.at<Vec3b>(Point(x, y));
+			}
 		imshow(titleDest, imgDispDest);
 	}
 
-	void onMouse( int event, int x, int y, int, void* )
+	void onMouseSrc( int event, int x, int y, int, void* )
 	{
 		int bo = 0;
 		switch(event)
@@ -65,6 +80,36 @@ namespace poi
 		}
 	}
 
+	void poi_run();
+
+	void onMouseDest( int event, int x, int y, int, void* )
+	{
+		int bo = 0;
+		switch(event)
+		{
+		case CV_EVENT_LBUTTONDOWN:
+			bo = 1;
+			mousex = x, mousey = y;
+			moused = 1;
+			break;
+		case CV_EVENT_LBUTTONUP:
+			bo = 0;
+			moused = 0;
+			break;
+		case CV_EVENT_MOUSEMOVE:
+			bo = moused;
+			break;
+		}
+		if( bo )
+		{
+			destOffsetX += x - mousex;
+			destOffsetY += y - mousey;
+			mousex = x, mousey = y;
+			poi_run();
+			redraw();
+		}
+	}
+
 	void poi_init(CString fsrc, CString fdest)
 	{
 		char tmps[256];
@@ -84,7 +129,8 @@ namespace poi
 		namedWindow(titleSrc , WINDOW_NORMAL ); // Create a window for display.
 		namedWindow(titleDest , WINDOW_NORMAL ); // Create a window for display.
 
-		setMouseCallback( titleSrc, onMouse, 0 );
+		setMouseCallback( titleSrc, onMouseSrc, 0 );
+		setMouseCallback( titleDest, onMouseDest, 0 );
 		
 		for(int i = 0; i < srcw; ++i)
 			for(int j = 0; j < srch; ++j)
@@ -94,12 +140,18 @@ namespace poi
 			}
 		imgDispSrc = imgSrc.clone();
 		imgDispDest = imgDest.clone();
+		res = imgSrc.clone();
 		redraw();
 	}
 
 	void poi_work(int _mode)
 	{
 		mode = _mode;
+		poi_run();
+	}
+
+	void poi_run()
+	{
 	}
 
 }
